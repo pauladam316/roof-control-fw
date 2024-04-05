5b6d84a535a71697b21f0f09df17cdd2931bd867
pauladam316 <pauladam316@gmail.com>
2024-04-05 08:59:16 -0400

    add command interface

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@
 #define RAISE_1_PIN 2
 #define RAISE_2_PIN 3
 
-#define LOWER_1_PIN 10
-#define LOWER_2_PIN 11
+#define LOWER_1_PIN 11
+#define LOWER_2_PIN 12
 
 #define ENGAGE_LOCK_PIN 8
 #define DISENGAGE_LOCK_PIN 9
@@ -23,6 +23,7 @@
 #define CMD_ENGAGE_LOCK 0x12
 #define CMD_DISENGAGE_LOCK 0x34
 #define STOP_LOCK 0x56
+#define CMD_HEARTBEAT 0xF1
 
 #define uint unsigned int
 
@@ -30,6 +31,8 @@
 
 #define NUM_ANALOG_SENSORS 3
 #define NUM_DIGITAL_SENSORS 4
+
+long cmd_timer = 0;
 
 class AnalogSensor {
   public:
@@ -68,32 +71,67 @@ typedef enum {
 
 namespace Roof {
   MotionState roof_state = STATE_UNKNOWN;
+  
 }
+
 namespace Lock {
   MotionState lock_state = STATE_UNKNOWN;
+  long timer = 0;
 }
 
 namespace Lock {
 
   void raise() {
-    if (lock_state != STATE_RAISED && Roof::roof_state == STATE_RAISED) {
+    if (lock_state != STATE_RAISED) {
       digitalWrite(DISENGAGE_LOCK_PIN, LOW);
       digitalWrite(ENGAGE_LOCK_PIN, HIGH);
       lock_state = STATE_RAISING;
+      timer = millis();
     }
   }
   void lower() {
-    if (lock_state != STATE_RAISED && Roof::roof_state == STATE_RAISED) {
+    if (lock_state != STATE_LOWERED) {
       digitalWrite(ENGAGE_LOCK_PIN, LOW);
       digitalWrite(DISENGAGE_LOCK_PIN, HIGH);
       lock_state = STATE_LOWERING;
+      timer = millis();
+    }
+  }
+  void stop() {
+    digitalWrite(ENGAGE_LOCK_PIN, LOW);
+    digitalWrite(DISENGAGE_LOCK_PIN, LOW);
+    lock_state = STATE_UNKNOWN;
+  }
+  void complete_raise() {
+    digitalWrite(ENGAGE_LOCK_PIN, LOW);
+    digitalWrite(DISENGAGE_LOCK_PIN, LOW);
+    lock_state = STATE_RAISED;
+  }
+
+  void complete_lower() {
+    digitalWrite(ENGAGE_LOCK_PIN, LOW);
+    digitalWrite(DISENGAGE_LOCK_PIN, LOW);
+    lock_state = STATE_LOWERED;
+  }
+
+  void update_state() {
+    if (lock_state == STATE_RAISING || lock_state == STATE_LOWERING) {
+
+      if (((millis()-timer) / 1000) > 10)
+      {
+        if (lock_state == STATE_RAISING) {
+          complete_raise();
+        }
+        else if (lock_state == STATE_LOWERING) {
+          complete_lower();
+        }
+      }
     }
   }
 
 }
 
 namespace Roof {
-
   void raise() {
     if (roof_state != STATE_RAISED && (Lock::lock_state == STATE_LOWERED || Lock::lock_state == STATE_UNKNOWN)) {
       digitalWrite(RAISE_1_PIN, HIGH);
@@ -101,6 +139,7 @@ namespace Roof {
       digitalWrite(LOWER_1_PIN, LOW);
       digitalWrite(LOWER_2_PIN, LOW);
       roof_state = STATE_RAISING;
+     
     }
   }
   void lower() {
@@ -112,12 +151,52 @@ namespace Roof {
       roof_state = STATE_LOWERING;
     }
   }
+  void stop() {
+    digitalWrite(RAISE_1_PIN, LOW);
+    digitalWrite(RAISE_2_PIN, LOW);
+    digitalWrite(LOWER_1_PIN, LOW);
+    digitalWrite(LOWER_2_PIN, LOW);
+    roof_state = STATE_UNKNOWN;
+  }
+  void complete_raise() {
+    digitalWrite(RAISE_1_PIN, LOW);
+    digitalWrite(RAISE_2_PIN, LOW);
+    digitalWrite(LOWER_1_PIN, LOW);
+    digitalWrite(LOWER_2_PIN, LOW);
+    roof_state = STATE_RAISED;
+  }
+
+  void complete_lower() {
+    digitalWrite(RAISE_1_PIN, LOW);
+    digitalWrite(RAISE_2_PIN, LOW);
+    digitalWrite(LOWER_1_PIN, LOW);
+    digitalWrite(LOWER_2_PIN, LOW);
+    roof_state = STATE_LOWERED;
+  }
+
+  void update_state() {
+    //TODO: if in lowered or raised and switches aren't triggered, move to unknown
+    if (roof_state != STATE_LOWERING && (digitalRead(LIMIT_U1_PIN) == HIGH || digitalRead(LIMIT_U2_PIN) == HIGH)){
+      complete_raise();
+    }
+    if (roof_state != STATE_RAISING && (digitalRead(LIMIT_L1_PIN) == HIGH || digitalRead(LIMIT_L2_PIN) == HIGH)){
+      complete_lower();
+    }
+    if (roof_state == STATE_LOWERED && (digitalRead(LIMIT_L1_PIN) == LOW && digitalRead(LIMIT_L2_PIN) == LOW)) {
+      roof_state = STATE_UNKNOWN;
+    }
+    if (roof_state == STATE_RAISED && (digitalRead(LIMIT_U1_PIN) == LOW && digitalRead(LIMIT_U2_PIN) == LOW)) {
+      roof_state = STATE_UNKNOWN;
+    }
+    if ((millis()-cmd_timer) > 300) {
+      //stop();
+    }
+  }
 
 }
 typedef enum {
     STATE_IDLE,
     STATE_READING_SYNC,
-    STATE_READING_LENGTH,
     STATE_READING_PAYLOAD,
 } CommandReadState;
 
@@ -129,12 +208,13 @@ AnalogSensor voltage_5v = AnalogSensor(VOLTAGE_5V_PIN, 0.009775171065494, 0);
 AnalogSensor voltage_12v = AnalogSensor(VOLTAGE_12V_PIN, 0.02117953730857, 0);
 AnalogSensor analog_sensors[NUM_ANALOG_SENSORS] = {h_bridge_current, voltage_5v, voltage_12v};
 
-int limit_switches[NUM_DIGITAL_SENSORS] = {RAISE_1_PIN, RAISE_2_PIN, LOWER_1_PIN, LOWER_2_PIN};
+int limit_switches[NUM_DIGITAL_SENSORS] = {LIMIT_U1_PIN, LIMIT_U2_PIN, LIMIT_L1_PIN, LIMIT_L2_PIN};
 
 byte serial_buffer[100] = {0};
 unsigned int buff_idx = 0;
 uint sync_bytes_read = 0;
 uint payload_length = 0;
+
 
 void setup() {
   pinMode(LIMIT_U1_PIN, INPUT);
@@ -147,19 +227,39 @@ void setup() {
   pinMode(LOWER_1_PIN, OUTPUT);
   pinMode(LOWER_2_PIN, OUTPUT);
 
+  pinMode(LIMIT_U1_PIN, INPUT);
+  pinMode(LIMIT_U2_PIN, INPUT);
+  pinMode(LIMIT_L1_PIN, INPUT);
+  pinMode(LIMIT_L2_PIN, INPUT);
+
   pinMode(ENGAGE_LOCK_PIN, OUTPUT);
   pinMode(DISENGAGE_LOCK_PIN, OUTPUT);
 
   Serial.begin(57600);
 
 }
-void parse_command(byte* buffer, uint length) {
-  if (length != 1) {
-    return;
-  }
-  switch (buffer[0]) {
+void parse_command(byte data) {
+  switch (data) {
     case CMD_RAISE_ROOF:
       Roof::raise();
+      break;
+    case CMD_LOWER_ROOF:
+      Roof::lower();
+      break;
+    case CMD_STOP_ROOF:
+      Roof::stop();
+      break;
+    case CMD_ENGAGE_LOCK:
+      Lock::raise();
+      break;
+    case CMD_DISENGAGE_LOCK:
+      Lock::lower();
+      break;
+    case STOP_LOCK:
+      Lock::stop();
+      break;
+    case CMD_HEARTBEAT:
+      cmd_timer = millis();
       break;
   }
 }
@@ -182,12 +282,16 @@ void send_telemetry() {
       Serial.write(0);
     }
   }
+  Serial.write(static_cast<uint8_t>(Roof::roof_state));
+  Serial.write(static_cast<uint8_t>(Lock::lock_state));
 }
 
 void loop() {
 
   //TODO: stop commands if commands are not sent
   send_telemetry();
+  Lock::update_state();
+  Roof::update_state();
   if (Serial.available() > 0) {
     // Read the incoming byte:
     byte data = Serial.read();
@@ -206,21 +310,13 @@ void loop() {
         else {
           sync_bytes_read ++;
           if (sync_bytes_read == 3) {
-            command_read_state = STATE_READING_LENGTH;
+            command_read_state = STATE_READING_PAYLOAD;
           }
         }
         break;
-      case STATE_READING_LENGTH:
-        payload_length = data;
-        command_read_state = STATE_READING_PAYLOAD;
-        break;
       case STATE_READING_PAYLOAD:
-        serial_buffer[buff_idx]= data;
-        buff_idx++;
-        if (buff_idx == payload_length) {
-          command_read_state = STATE_IDLE;
-          parse_command(serial_buffer, payload_length);
-        }
+        command_read_state = STATE_IDLE;
+        parse_command(data);
         
         break;
     }
@@ -228,6 +324,6 @@ void loop() {
     // Echo the received byte back to the serial   port
     //Serial.println(receivedChar);
   }
-  delay(100);
+  delay(1);
 
 }
